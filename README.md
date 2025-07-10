d490cad289830622549c9243db395d67f33f50f3
anandhunv <anandhunv05@gmail.com>
2025-07-10 15:02:02 +0530

    project base + tailwindcss + daisyui + navbar

---
+++ README.md
@@ -0,0 +1,5 @@
+create vite + react project 
+remove app.css and unnessary code 
+install tailwind css
+install diasy ui
+add navbar component from daisy to project
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className='text-4xl text-red-700'>
+
+    <Navbar/>
+
+    </div>
+  )
+}
+
+export default App 
+
+
